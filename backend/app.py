f3eeb8ca124b61a57720103d9e4fdcabc09c7aa6
Ajinkya mane <126250012+ajinkyaXhub@users.noreply.github.com>
2026-07-07 16:02:04 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,13 @@ from query import query_knowledge_base
 from flask import send_from_directory
 
 app = Flask(__name__)
-CORS(app)  # Allows React (running on port 3000) to talk to Flask (port 5000)
+CORS(app, resources={
+    r"/*": {
+        "origins": ["https://context-bot-rag.vercel.app", "http://localhost:3000"],
+        "methods": ["GET", "POST", "OPTIONS"],
+        "allow_headers": ["Content-Type"]
+    }
+})
 
 UPLOAD_FOLDER = "./uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
